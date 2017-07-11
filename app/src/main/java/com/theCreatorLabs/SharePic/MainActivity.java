e7a7fc61b9ecc459c2cf970317a2186b1d41ee9d
AkshathKotegar <itsakshath@gmail.com>
2017-07-11 11:51:25 +0530

    changes done

--- app/src/main/java/com/theCreatorLabs/SharePic/MainActivity.java
+++ app/src/main/java/com/theCreatorLabs/SharePic/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 requestForSpecificPermission();
             }
         }
-
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
